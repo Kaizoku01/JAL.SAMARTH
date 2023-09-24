d89d9623f0a4e52598a4adf8cd4d5ef15e5c1cac
Rhythm Shandlya <armaanbgp@gmail.com>
2023-09-24 16:05:28 +0530

    ANDROID SUPPORT

---
+++ app/android/src/styles/colors.js
@@ -0,0 +1,23 @@
+const blueVersion = {
+  primary: '#FF5733',
+  primaryLight: '#FFA07A',
+  primaryGradientStart: '#FF4500',
+  primaryGradientEnd: '#FF6347',
+  secondaryGradientStart: '#FF5733',
+  secondaryGradientEnd: '#FF5733',
+  profileGradientStart: '#FF6B6B',
+  profileGradientEnd: '#FFD700',
+  secondary: '#FF5733',
+  grey: '#acacac',
+  gray: '#5f5f5f',
+  darkGray: '#4d4d4d',
+  lightGray: '#9b9b9b',
+  white: '#ffffff',
+  blue: '#5A81F7',
+  bluish: '#F1F1F7',
+  black: '#000000',
+  green: '#6DD0A3',
+  yellow: '#ffc247',
+};
+
+export default blueVersion;
